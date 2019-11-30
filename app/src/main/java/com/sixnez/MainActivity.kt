608ca9d2bedc691d6f3decc86d4053ac3044385a
thibaultChatelain <thibault.chatelain@etu.univ-lyon1.fr>
2019-11-30 17:46:25 +0100

    Navigation par fragments

--- app/src/main/java/com/sixnez/MainActivity.kt
+++ app/src/main/java/com/sixnez/MainActivity.kt
@@ -2,13 +2,17 @@ package com.sixnez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import android.widget.Toast
 
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var drawer: DrawerLayout
 
@@ -21,6 +25,9 @@ class MainActivity : AppCompatActivity() {
 
         drawer = findViewById(R.id.drawer_layout)
 
+        var navigationView : NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
         var toggle : ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer.addDrawerListener(toggle)
         toggle.syncState()
@@ -32,5 +39,23 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.nav_home -> supportFragmentManager.beginTransaction().replace(
+                R.id.fragment,
+                LoginFragment()
+            ).commit()
+
+            R.id.nav_search -> supportFragmentManager.beginTransaction().replace(
+                R.id.fragment,
+                RegisterFragment()
+            ).commit()
+        }
+
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
